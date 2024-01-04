79bee6befb431f2026abf7f6ff9aa4fd8042f53b
herval <hervalfreire@gmail.com>
2024-01-04 13:17:04 -0300

    use chroma instead of implementing stuff by hand + add persistence

---
+++ lib/chroma.py
@@ -0,0 +1,84 @@
+from typing import Optional
+
+import chromadb
+
+from lib import Engine
+from chromadb.utils import embedding_functions
+
+
+class ChromaDbEngine(Engine):
+    def __init__(self, embedding_function, threshold, cache_path=None):
+        """
+        :param model_name: sentence_transformer model to use
+        :param threshold: minimum similarity score to match a route
+        """
+        self.client = chromadb.PersistentClient(cache_path) if cache_path else chromadb.EphemeralClient()
+        self.transformer_fn = embedding_function  # embedding_functions.SentenceTransformerEmbeddingFunction(model_name=model_name)
+        self.threshold = threshold
+        self.collection = self.client.get_or_create_collection(
+            name="sentences",
+            embedding_function=self.transformer_fn)
+
+    def encode(self, routes):
+        docs = []
+        meta = []
+
+        for r in routes:
+            for s in r.sentences:
+                docs.append(s)
+                meta.append({"name": r.name})
+
+        self.collection.upsert(
+            documents=docs,
+            metadatas=meta,
+            ids=docs
+        )
+
+    def match(self, query):
+        res = self.collection.query(
+            query_texts=[query],
+            n_results=3,
+        )
+
+        if len(res) == 0 or res['distances'][0][0] < self.threshold:
+            return None
+
+        return res['metadatas'][0][0]['name']
+
+
+class SentenceTransformer(ChromaDbEngine):
+    def __init__(self,
+                 threshold=0,
+                 cache_path=None,
+                 model_name: str = "all-distilroberta-v1"):
+        super().__init__(
+            cache_path=cache_path,
+            threshold=threshold,
+            embedding_function=embedding_functions.SentenceTransformerEmbeddingFunction(
+                model_name=model_name
+            ))
+
+
+class OpenAI(ChromaDbEngine):
+    def __init__(self,
+                 cache_path=None,
+                 threshold=0,
+                 api_key: Optional[str] = None,
+                 model_name: str = "text-embedding-ada-002",
+                 organization_id: Optional[str] = None,
+                 api_base: Optional[str] = None,
+                 api_type: Optional[str] = None,
+                 api_version: Optional[str] = None,
+                 deployment_id: Optional[str] = None):
+        super().__init__(
+            cache_path=cache_path,
+            embedding_function=embedding_functions.OpenAIEmbeddingFunction(
+                api_key=api_key,
+                model_name=model_name,
+                organization_id=organization_id,
+                api_base=api_base,
+                api_type=api_type,
+                api_version=api_version,
+                deployment_id=deployment_id
+            ),
+            threshold=threshold)
--- test/test_router.py
+++ test/test_router.py
@@ -1,16 +1,18 @@
+import os
 import unittest
 
 from lib import Router, Route
-from lib.transformers_engine import SentenceTransformersEngine
+from lib.chroma import SentenceTransformer, OpenAI
 
 
 class TestRouter(unittest.TestCase):
 
     def test_transformer_router(self):
         router = Router(
-            engine=SentenceTransformersEngine(
+            engine=SentenceTransformer(
+                cache_path="./test_transformer_cache",
+                threshold=0.4,
                 model_name='all-distilroberta-v1',
-                threshold=0.3,
             ),
             routes=[
                 Route(
@@ -46,10 +48,54 @@ class TestRouter(unittest.TestCase):
             ]
         )
 
-        assert router.match('make this big') == 'upscale'
-        assert router.match('I can has big image?') == 'upscale'
-        assert router.match('enlarge!') == 'upscale'
-        assert router.match('is it possible to make this better in terms of graphical quality') == 'upscale'
+        self.assertEquals(router.match('make this big'), 'upscale')
+        self.assertEquals(router.match('I can has big image?'), 'upscale')
+        self.assertEquals(router.match('enlarge!'), 'upscale')
+        self.assertEquals(router.match('is it possible to make this better in terms of graphical quality'), 'upscale')
 
-        assert router.match('i said i want black and white!!!') == 'edit'
-        assert router.match('cut it in half') == 'edit'
+        self.assertEquals(router.match('i said i want black and white!!!'), 'edit')
+        self.assertEquals(router.match('cut it in half'), 'edit')
+
+    def test_openai_router(self):
+        router = Router(
+            engine=OpenAI(
+                cache_path="./test_openai_cache",
+                api_key=os.environ.get('OPENAI_API_KEY'),
+            ),
+            routes=[
+                Route(
+                    name='upscale',
+                    sentences=[
+                        'upscale the image',
+                        'increase image resolution',
+                        'increase pixel count',
+                        'increase the size of an image',
+                        'increase the resolution of the image',
+                    ]
+                ),
+                Route(
+                    name='edit',
+                    sentences=[
+                        'edit image',
+                        'rotate image',
+                        'flip image',
+                        'resize image',
+                        'adjust contrast',
+                        'adjust saturation',
+                        'adjust brightness',
+                        'change or adjust colors, for instance make entire image use a specific color palette, make images black and white, etc',
+                        'color balance',
+                        'change image format',
+                        'change dimensions',
+                        'change size',
+                        'crop image',
+                    ]
+                )
+            ]
+        )
+        self.assertEquals(router.match('I can has big image?'), 'upscale')
+        self.assertEquals(router.match('enlarge!'), 'upscale')
+        self.assertEquals(router.match('is it possible to make this better in terms of graphical quality'), 'upscale')
+
+        self.assertEquals(router.match('i said i want black and white!!!'), 'edit')
+        self.assertEquals(router.match('cut it in half'), 'edit')
