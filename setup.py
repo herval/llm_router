560261a321c62d1431ea73d9108875210832ae41
herval <hervalfreire@gmail.com>
2024-01-04 13:29:26 -0300

    wip package

--- setup.py
+++ setup.py
@@ -2,10 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name='llm-router',
-    version='0.1.0',
+    version='0.1.1',
     author='herval',
     author_email='hervalfreire@gmail.com',
     description='Quick LLM routing using Embeddings',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
     packages=find_packages(),
     classifiers=[
         'Programming Language :: Python :: 3',
